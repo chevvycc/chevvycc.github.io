60c46e459cd714c4b4e6a1eaf8d8fbe246e1ae3e
mcassagnes <mathieu.cassagnes.1@ulaval.ca>
2015-02-04 00:53:01 -0500

    ok

--- app/app.js
+++ app/app.js
@@ -18,7 +18,7 @@ app.get('/movies/:id', function(req, res) {
             return;
         }
     }
-    res.send("aucun film trouvé");
+    res.send("Aucun film trouvé");
 });
 
 app.post('/movies', function(req, res) {
@@ -36,14 +36,33 @@ app.post('/movies', function(req, res) {
     res.send(newMovie);
 });
 
+app.put('/movies/:id', function(req, res) {
+    var editedMovie = req.body;
+    editedMovie.id = req.params.id;
+
+    for (var i = 0; i < movies.length; i++) {
+        if (movies[i].id === editedMovie.id) {
+            movies[i] = editedMovie;
+            res.send("Film avec l'id " + editedMovie.id + " modifié");
+            return;
+        }
+    }
+    res.send("Film avec l'id " + editedMovie.id + " introuvable")
+});
+
+app.get('*', function(req, res) {
+    res.redirect('/');
+    //res.sendFile(__dirname + '/redirect.html');
+});
+
 app.listen(5000, function() {
     console.log("Rotten Vegetables app running at localhost:" + 5000);
 });
 
 var movies = [
-    {"id" : 1, "title": "The Shining", "genre": "horror"},
-    {"id" : 2, "title": "Fargo", "genre": "drama"},
-    {"id" : 3, "title": "TED", "genre": "comedy"},
-    {"id" : 4, "title": "Armageddon", "genre": "action"},
-    {"id" : 1, "title": "Harry Potter", "genre": "fantasy"}
+    {"id" : "1423027070731", "title": "The Shining", "genre": "horror"},
+    {"id" : "1423027078981", "title": "Fargo", "genre": "drama"},
+    {"id" : "1423027089022", "title": "TED", "genre": "comedy"},
+    {"id" : "1423027096474", "title": "Armageddon", "genre": "action"},
+    {"id" : "1423027107513", "title": "Harry Potter", "genre": "fantasy"}
 ];
